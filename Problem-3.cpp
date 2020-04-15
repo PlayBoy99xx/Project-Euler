e2a75fc98ae0a5e9292779fdc91d879e9184eb49
PlayBoy99xx <33063661+PlayBoy99xx@users.noreply.github.com>
2020-04-15 13:13:11 +0500

    Create Problem-3.cpp

---
+++ Problem-3.cpp
@@ -0,0 +1,36 @@
+/*
+    Problem 3:
+                Largest Prime Factor
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool isPrime = false;
+    long long int num = 600851475143;
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0) //Checking if number if factor or not
+        {
+            // cout << i << endl;
+            for (int j = 2; j <= i - 1; j++) //Checking if that factor is prime or not
+            {
+                // cout << i << endl;
+                if ((i % j == 0))
+                {
+                    isPrime = false;
+                    break;
+                }
+                else
+                {
+                    isPrime = true;
+                }
+            }
+            if (isPrime)
+            {
+                cout << i << endl;
+            }
+        }
+    }
+}
